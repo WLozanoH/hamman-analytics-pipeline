62013e3fa5495381e2feb564500916a912821248
Wilmer Lozano <wglozanoh@gmail.com>
2026-03-31 17:25:32 -0500

    chore(project): improve path configuration and package structure

--- src/config.py
+++ src/config.py
@@ -1,7 +1,15 @@
-from dotenv import load_dotenv
-import os
+from dotenv import load_dotenv                              # Importa función para leer el archivo .env
+import os                                                   # Importa módulo para trabajar con variables del sistema
+from pathlib import Path
 
 load_dotenv()       # Carga las variables del archivo .env
 
-FILE_PATH = os.getenv("FILE_PATH")
-OUTPUT_PATH = os.getenv("OUTPUT_PATH")
+# raíz del proyecto
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# variables del .env
+FILE_PATH = BASE_DIR / os.getenv("FILE_PATH")
+OUTPUT_PATH = BASE_DIR / os.getenv("OUTPUT_PATH")
+
+if not FILE_PATH.exists():
+    raise FileNotFoundError(f"No se encontró el archivo de entrada: {FILE_PATH}")
