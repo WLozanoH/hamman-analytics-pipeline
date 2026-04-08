e2f7f8f5238e0a37698b4d7915aaefcb43460cb0
Wilmer Lozano <wglozanoh@gmail.com>
2026-04-07 20:21:14 -0500

    feat(sql): add SQL layer structure and database configuration

--- src/config.py
+++ src/config.py
@@ -11,5 +11,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 FILE_PATH = BASE_DIR / os.getenv("FILE_PATH")
 OUTPUT_PATH = BASE_DIR / os.getenv("OUTPUT_PATH")
 
+SQL_SERVER = os.getenv("SQL_SERVER")
+SQL_DATABASE = os.getenv("SQL_DATABASE")
+SQL_DRIVER = os.getenv("SQL_DRIVER", "ODBC Driver 17 for SQL Server")
+SQL_TRUST_CERT = os.getenv("SQL_TRUST_CERT", "yes")
+SQL_USE_WINDOWS_AUTH = os.getenv("SQL_USE_WINDOWS_AUTH", "yes")
+
+SQL_USERNAME = os.getenv("SQL_USERNAME")
+SQL_PASSWORD = os.getenv("SQL_PASSWORD")
+
+
 if not FILE_PATH.exists():
     raise FileNotFoundError(f"No se encontró el archivo de entrada: {FILE_PATH}")
